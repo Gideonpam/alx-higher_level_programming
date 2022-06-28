8fa1a6cd88572964eb08fa446e679374e6e2c56c
Gideonpam <gwompamgideon@gmail.com>
2022-06-28 17:49:46 +0100

    if, else, functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,20 +9,31 @@ num_str = repr(number)
 last_digit_str = num_str[-1]
 # 3. Convert the last digit string to an integer:
 last_digit = int(last_digit_str)
+message = "Last digit of "
+# if last_digit > 5:
+#     if number > 0:
+#         print(f"{message} {number} is {last_digit_str} "
+#         "and is greater than 5")
+#     else:
+#         print(f"{message} {number} is -{last_digit_str} "
+#         "and is less than 6 and not 0")
+# elif last_digit == 0:
+#     print(f"{message} {number} is {last_digit_str} and is 0")
+# elif last_digit < 6 and last_digit != 0:
+#     if number > 0:
+#         print(f"{message} {number} is {last_digit_str} "
+#         "and is less than 6 and not 0")
+#     else:
+#         print(f"{message} {number} is -{last_digit_str} "
+#         "and is less than 6 and not 0")
 
+if number < 0:
+    last_digit = -last_digit
+print("{}{} is {} and is ".format(message, number, last_digit), end="")
 if last_digit > 5:
-    if number > 0:
-        print(f"Last digit of {number} is {last_digit_str} "
-        "and is greater than 5")
-    else:
-        print(f"Last digit of {number} is -{last_digit_str} "
-        "and is less than 6 and not 0")
+    print("greater than 5")
 elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit_str} and is 0")
-elif last_digit < 6 and last_digit != 0:
-    if number > 0:
-        print(f"Last digit of {number} is {last_digit_str} "
-        "and is less than 6 and not 0")
-    else:
-        print(f"Last digit of {number} is -{last_digit_str} "
-        "and is less than 6 and not 0")
+    print("0")
+else:
+    print("less than 6 and not 0")
+
